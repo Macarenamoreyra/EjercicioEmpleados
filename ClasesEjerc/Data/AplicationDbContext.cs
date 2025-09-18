4df3d8f35a63a21d83accd7957c2cc97dffd541f
Macarenamoreyra <macarenamoreyra06@gmail.com>
2025-09-17 21:04:53 -0300

    Agregar archivos de proyecto.

---
+++ ClasesEjerc/Data/AplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClasesEjerc.Data
+{
+    public class AplicationDbContext : DbContext
+    {
+        public DbSet<Empleado> Empleado { get; set; }
+        public DbSet<Departamento> Departamento { get; set; }
+        
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=(localdb)\\MSSQLLocalDB;Database=PruebaEF;Trusted_Connection=True;TrustServerCertificate=True;"
+            );
+        }
+    }
+}
